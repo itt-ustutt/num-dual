5a46d942a23fe1cc0452150eadd9d1cf851c8846
Philipp Rehner <prehner@ethz.ch>
2021-12-15 18:17:31 +0100

    add eigenvalue calculation and fix norm

--- src/linalg/mod.rs
+++ src/linalg/mod.rs
@@ -149,20 +149,136 @@ impl<T: DualNum<F>, F: Float> LU<T, F> {
 }
 
 pub fn norm<T: DualNum<F>, F: Float>(x: &Array1<T>) -> T {
-    x.iter().fold(T::zero(), |acc, &x| acc + x)
+    x.iter().fold(T::zero(), |acc, &x| acc + x * x).sqrt()
 }
 
-pub fn smallest_ev<T: DualNum<F>, F: Float>(a: &Array2<T>) -> Result<(T, Array1<T>), LinAlgError> {
-    unimplemented!()
-    // let (e, vecs) = a.eigh(UPLO::Upper)?;
-    // let i_min = e.map(Dual64::re).argmin().unwrap();
-    // Ok((e[i_min], vecs.row(i_min).to_owned()))
+pub fn smallest_ev<T: DualNum<F>, F: Float>(a: Array2<T>) -> (T, Array1<T>) {
+    let (e, vecs) = jacobi_eigenvalue(a, 200);
+    (e[0], vecs.column(0).to_owned())
+}
+
+pub fn jacobi_eigenvalue<T: DualNum<F>, F: Float>(
+    mut a: Array2<T>,
+    max_iter: usize,
+) -> (Array1<T>, Array2<T>) {
+    let n = a.shape()[0];
+
+    let mut v = Array2::eye(n);
+    let mut d = a.diag().to_owned();
+
+    let mut bw = d.clone();
+    let mut zw = Array1::zeros(n);
+
+    for it_num in 0..max_iter {
+        let mut thresh = F::zero();
+        for j in 0..n {
+            for i in 0..j {
+                thresh = thresh + a[(i, j)].re().powi(2);
+            }
+        }
+        thresh = thresh.sqrt() / F::from(n).unwrap();
+
+        if thresh.is_zero() {
+            break;
+        }
+
+        for p in 0..n {
+            for q in p + 1..n {
+                let gapq = a[(p, q)].abs() * F::from(10.0).unwrap();
+                let termp = gapq + d[p].abs();
+                let termq = gapq + d[q].abs();
+
+                if 4 < it_num && termp == d[p].abs() && termq == d[q].abs() {
+                    a[(p, q)] = T::zero();
+                } else if thresh <= a[(p, q)].re().abs() {
+                    let h = d[q] - d[p];
+                    let term = h.abs() + gapq;
+
+                    let t = if term == h.abs() {
+                        a[(p, q)] / h
+                    } else {
+                        let theta = h * F::from(0.5).unwrap() / a[(p, q)];
+                        let mut t = (theta.abs() + (theta * theta + F::one()).sqrt()).recip();
+                        if theta.is_negative() {
+                            t = -t;
+                        }
+                        t
+                    };
+
+                    let c = (t * t + F::one()).sqrt().recip();
+                    let s = t * c;
+                    let tau = s / (c + F::one());
+                    let h = t * a[(p, q)];
+
+                    zw[p] -= h;
+                    zw[q] += h;
+                    d[p] -= h;
+                    d[q] += h;
+
+                    a[(p, q)] = T::zero();
+
+                    for j in 0..p {
+                        let g = a[(j, p)];
+                        let h = a[(j, q)];
+                        a[(j, p)] = g - s * (h + g * tau);
+                        a[(j, q)] = h + s * (g - h * tau);
+                    }
+
+                    for j in p + 1..q {
+                        let g = a[(p, j)];
+                        let h = a[(j, q)];
+                        a[(p, j)] = g - s * (h + g * tau);
+                        a[(j, q)] = h + s * (g - h * tau);
+                    }
+
+                    for j in q + 1..n {
+                        let g = a[(p, j)];
+                        let h = a[(q, j)];
+                        a[(p, j)] = g - s * (h + g * tau);
+                        a[(q, j)] = h + s * (g - h * tau);
+                    }
+
+                    for j in 0..n {
+                        let g = v[(j, p)];
+                        let h = v[(j, q)];
+                        v[(j, p)] = g - s * (h + g * tau);
+                        v[(j, q)] = h + s * (g - h * tau);
+                    }
+                }
+            }
+        }
+
+        bw += &zw;
+        d.assign(&bw);
+        zw.fill(T::zero());
+    }
+
+    for k in 0..n - 1 {
+        let mut m = k;
+
+        for l in k + 1..n {
+            if d[l].re() < d[m].re() {
+                m = l;
+            }
+        }
+
+        if m != k {
+            d.swap(m, k);
+
+            for l in 0..n {
+                v.swap((l, m), (l, k));
+            }
+        }
+    }
+
+    (d, v)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::Dual64;
+    use approx::assert_abs_diff_eq;
     use ndarray::{arr1, arr2};
 
     #[test]
@@ -195,6 +311,66 @@ mod tests {
         assert_eq!(
             (x[0].re, x[0].eps[0], x[1].re, x[1].eps[0]),
             (1.0, 2.0, 2.0, 1.0)
+        );
+    }
+
+    #[test]
+    fn test_eig_f64_2() {
+        let a = arr2(&[[2.0, 2.0], [2.0, 5.0]]);
+        let (l, v) = jacobi_eigenvalue(a.clone(), 200);
+        let av = a.dot(&v);
+        println!("{} {}", l, v);
+        assert_abs_diff_eq!(av[(0, 0)], (l[0] * v[(0, 0)]), epsilon = 1e-14);
+        assert_abs_diff_eq!(av[(1, 0)], (l[0] * v[(1, 0)]), epsilon = 1e-14);
+        assert_abs_diff_eq!(av[(0, 1)], (l[1] * v[(0, 1)]), epsilon = 1e-14);
+        assert_abs_diff_eq!(av[(1, 1)], (l[1] * v[(1, 1)]), epsilon = 1e-14);
+    }
+
+    #[test]
+    fn test_eig_f64_3() {
+        let a = arr2(&[[2.0, 2.0, 7.0], [2.0, 5.0, 9.0], [7.0, 9.0, 2.0]]);
+        let (l, v) = jacobi_eigenvalue(a.clone(), 200);
+        let av = a.dot(&v);
+        println!("{} {}", l, v);
+        for i in 0..3 {
+            for j in 0..3 {
+                assert_abs_diff_eq!(av[(i, j)], (l[j] * v[(i, j)]), epsilon = 1e-14);
+            }
+        }
+    }
+
+    #[test]
+    fn test_eig_dual64() {
+        let a = arr2(&[
+            [Dual64::new_scalar(2.0, 1.0), Dual64::new_scalar(2.0, 2.0)],
+            [Dual64::new_scalar(2.0, 2.0), Dual64::new_scalar(5.0, 3.0)],
+        ]);
+        let (l, v) = jacobi_eigenvalue(a.clone(), 200);
+        let av = a.dot(&v);
+        println!("{} {}", l, v);
+        assert_abs_diff_eq!(av[(0, 0)].re, (l[0] * v[(0, 0)]).re, epsilon = 1e-14);
+        assert_abs_diff_eq!(av[(1, 0)].re, (l[0] * v[(1, 0)]).re, epsilon = 1e-14);
+        assert_abs_diff_eq!(av[(0, 1)].re, (l[1] * v[(0, 1)]).re, epsilon = 1e-14);
+        assert_abs_diff_eq!(av[(1, 1)].re, (l[1] * v[(1, 1)]).re, epsilon = 1e-14);
+        assert_abs_diff_eq!(
+            av[(0, 0)].eps[0],
+            (l[0] * v[(0, 0)]).eps[0],
+            epsilon = 1e-14
+        );
+        assert_abs_diff_eq!(
+            av[(1, 0)].eps[0],
+            (l[0] * v[(1, 0)]).eps[0],
+            epsilon = 1e-14
+        );
+        assert_abs_diff_eq!(
+            av[(0, 1)].eps[0],
+            (l[1] * v[(0, 1)]).eps[0],
+            epsilon = 1e-14
+        );
+        assert_abs_diff_eq!(
+            av[(1, 1)].eps[0],
+            (l[1] * v[(1, 1)]).eps[0],
+            epsilon = 1e-14
         );
     }
 }
