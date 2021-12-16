ebbb7827f2eb72c907189521f7a18f63e7ac2ba3
Gernot Bauer <bauer@itt.uni-stuttgart.de>
2021-12-16 16:45:55 +0100

    Adds build_wheel directory that is used to build the python wheel

---
+++ build_wheel/src/lib.rs
@@ -0,0 +1,8 @@
+use num_dual as num_dual_rs;
+use pyo3::prelude::*;
+
+/// Implementation of SI numbers.
+#[pymodule]
+pub fn num_dual(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    num_dual_rs::python::num_dual(_py, m)
+}
