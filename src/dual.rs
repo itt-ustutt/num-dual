d1354df9abed766ccb97401efc775c5eda537a9d
Cormac Relf <web@cormacrelf.net>
2023-05-03 01:44:22 +1000

    impl nalgebra::RealField/ComplexField for DualVec (cherry-pick start)

--- src/dual.rs
+++ src/dual.rs
@@ -1,4 +1,5 @@
 use crate::{Derivative, DualNum, DualNumFloat};
+use approx::{AbsDiffEq, RelativeEq, UlpsEq};
 use nalgebra::allocator::Allocator;
 use nalgebra::*;
 use num_traits::{Float, FloatConst, FromPrimitive, Inv, Num, One, Signed, Zero};
@@ -286,25 +287,20 @@ impl_dual!(DualVec, [eps], [D]);
 
 /**
  * The SimdValue trait is for rearranging data into a form more suitable for Simd,
- * and rearranging it back into a usable form.
+ * and rearranging it back into a usable form. It is not documented particularly well.
  *
- * The primary job of this SimdValue impl is to allow people to use `simba::simd::AutoSimd`,
- * which implements the `num` traits, as their T, with our F parameter
- * set to <T as SimdValue>::Element. The AutoSimd type stores short arrays of floats etc, but
- * needs to be treated like a scalar. Therefore you need to be able to split up any type that
- * contains an AutoSimd<[f32; 4]> into four of your wrapper type. Then you can do normal math
- * operations on each one without having to know that underneath, everything was stored in
- * batches of [f32; 4]. But if you _do_ want to take advantage of SIMD instructions,
- * specialized implementations of `SimdRealField` can choose *not* to decompose the wrapper of
- * [f32; 4] into * individual wrappers of f32, and instead do bulk operations on the [f32; 4]s.
- * That's the idea. SimdValue is plumbing to be able to plug any wrapper type in to use
- * the non-specialized math operations that you would * find * in `nalgebra::RealField`.
- * `RealField` provides the default implementation of `SimdRealField`'s methods.
+ * The primary job of this SimdValue impl is to allow people to use `simba::simd::f32x4` etc,
+ * instead of f32/f64. Those types implement nalgebra::SimdRealField/ComplexField, so they
+ * behave like scalars. When we use them, we would have `DualVec<f32x4, f32, N>` etc, with our
+ * F parameter set to <T as SimdValue>::Element. We will need to be able to split up that type
+ * into four of DualVec in order to get out of simd-land. That's what the SimdValue trait is for.
  *
- * Ultimately, if you want more than _mere plumbing compatibility_ with AutoSimd and the like,
- * then you would have to implement SimdRealField on DualVec in such a way that it uses
- * SIMD instructions on particular CPUs. That's future work for someone who finds num_dual is not
- * fast enough.
+ * Ultimately, someone will have to to implement SimdRealField on DualVec and call the
+ * simd_ functions of <T as SimdRealField>. That's future work for someone who finds
+ * num_dual is not fast enough.
+ *
+ * Unfortunately, doing anything with SIMD is blocked on
+ * <https://github.com/dimforge/simba/issues/44>.
  *
  */
 impl<T, D: Dim> nalgebra::SimdValue for DualVec<T, T::Element, D>
@@ -313,17 +309,14 @@ where
     T: DualNum<T::Element> + SimdValue + Scalar,
     T::Element: DualNum<T::Element> + Scalar,
 {
-    // Say T = AutoSimd<[f32; 4]>. T::Element is f32. T::SimdBool is AutoSimd<[bool; 4]>.
+    // Say T = simba::f32x4. T::Element is f32. T::SimdBool is AutoSimd<[bool; 4]>.
     // AutoSimd<[f32; 4]> stores an actual [f32; 4], i.e. four floats in one slot.
     // So our DualVec<AutoSimd<[f32; 4], f32, N> has 4 * (1+N) floats in it, stored in blocks of
-    // four. When we want to do ANY math on it, we need to break that type into
-    // FOUR of DualVec<f32, f32, N>; then we do math on it, then we bring it back together. The
-    // SimdValue trait lets other nalgebra code do this:
+    // four. When we want to do any math on it but ignore its f32x4 storage mode, we need to break
+    // that type into FOUR of DualVec<f32, f32, N>; then we do math on it, then we bring it back
+    // together.
     //
-    // 1. Ask us how many lanes to use (4, because AutoSimd<[f32; 4]> says "4 lanes please")
-    // 2. Extract the  i-th lane, for i < 4, returning DualVec<f32, f32, N>.
-    // 2.
-    //
+    // Hence this definition of Element:
     type Element = DualVec<T::Element, T::Element, D>;
     type SimdBool = T::SimdBool;
 
@@ -416,32 +409,32 @@ where
 }
 /// Like PartialEq, comparisons are only made based on the real part. This allows the code to follow the
 /// same execution path as real-valued code would.
-impl<T: DualNum<F> + approx::AbsDiffEq<Epsilon = F>, F: Float, D: Dim> approx::AbsDiffEq
+impl<T: DualNum<F> + approx::AbsDiffEq<Epsilon = T>, F: Float, D: Dim> approx::AbsDiffEq
     for DualVec<T, F, D>
 where
     DefaultAllocator: Allocator<T, D>,
 {
-    type Epsilon = F;
+    type Epsilon = Self;
     #[inline]
     fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
-        self.re.abs_diff_eq(&other.re, epsilon)
+        self.re.abs_diff_eq(&other.re, epsilon.re)
     }
 
     #[inline]
     fn default_epsilon() -> Self::Epsilon {
-        T::default_epsilon()
+        Self::from_re(T::default_epsilon())
     }
 }
 /// Like PartialEq, comparisons are only made based on the real part. This allows the code to follow the
 /// same execution path as real-valued code would.
-impl<T: DualNum<F> + approx::RelativeEq<Epsilon = F>, F: Float, D: Dim> approx::RelativeEq
+impl<T: DualNum<F> + approx::RelativeEq<Epsilon = T>, F: Float, D: Dim> approx::RelativeEq
     for DualVec<T, F, D>
 where
     DefaultAllocator: Allocator<T, D>,
 {
     #[inline]
     fn default_max_relative() -> Self::Epsilon {
-        T::default_max_relative()
+        Self::from_re(T::default_max_relative())
     }
 
     #[inline]
@@ -451,6 +444,472 @@ where
         epsilon: Self::Epsilon,
         max_relative: Self::Epsilon,
     ) -> bool {
-        self.re.relative_eq(&other.re, epsilon, max_relative)
+        self.re.relative_eq(&other.re, epsilon.re, max_relative.re)
+    }
+}
+impl<T: DualNum<F> + UlpsEq<Epsilon = T>, F: Float, const N: usize> UlpsEq for DualVec<T, F, N> {
+    #[inline]
+    fn default_max_ulps() -> u32 {
+        T::default_max_ulps()
+    }
+
+    #[inline]
+    fn ulps_eq(&self, other: &Self, epsilon: Self::Epsilon, max_ulps: u32) -> bool {
+        T::ulps_eq(&self.re, &other.re, epsilon.re, max_ulps)
+    }
+}
+
+impl<T, const N: usize> nalgebra::Field for DualVec<T, T::Element, N>
+where
+    T: DualNum<T::Element> + SimdValue,
+    T::Element: DualNum<T::Element> + Scalar + Float,
+{
+}
+
+use simba::scalar::{SubsetOf, SupersetOf};
+
+impl<TSuper, FSuper, T, F, const N: usize> SubsetOf<DualVec<TSuper, FSuper, N>> for DualVec<T, F, N>
+where
+    TSuper: DualNum<FSuper> + SupersetOf<T>,
+    T: DualNum<F>,
+{
+    #[inline(always)]
+    fn to_superset(&self) -> DualVec<TSuper, FSuper, N> {
+        let re = TSuper::from_subset(&self.re);
+        let eps = SVector::<TSuper, N>::from_subset(&self.eps);
+        DualVec {
+            re,
+            eps,
+            f: PhantomData,
+        }
+    }
+    #[inline(always)]
+    fn from_superset(element: &DualVec<TSuper, FSuper, N>) -> Option<Self> {
+        let re = TSuper::to_subset(&element.re)?;
+        let eps = SVector::<TSuper, N>::to_subset(&element.eps)?;
+        Some(Self {
+            re,
+            eps,
+            f: PhantomData,
+        })
+    }
+    #[inline(always)]
+    fn from_superset_unchecked(element: &DualVec<TSuper, FSuper, N>) -> Self {
+        let re = TSuper::to_subset_unchecked(&element.re);
+        let eps = SVector::<TSuper, N>::to_subset_unchecked(&element.eps);
+        Self {
+            re,
+            eps,
+            f: PhantomData,
+        }
+    }
+    #[inline(always)]
+    fn is_in_subset(element: &DualVec<TSuper, FSuper, N>) -> bool {
+        TSuper::is_in_subset(&element.re)
+            && <SVector<TSuper, N> as SupersetOf<SVector<T, N>>>::is_in_subset(&element.eps)
+    }
+}
+
+impl<TSuper, FSuper, const N: usize> SupersetOf<f32> for DualVec<TSuper, FSuper, N>
+where
+    TSuper: DualNum<FSuper> + SupersetOf<f32>,
+{
+    #[inline(always)]
+    fn is_in_subset(&self) -> bool {
+        self.re.is_in_subset()
+    }
+
+    #[inline(always)]
+    fn to_subset_unchecked(&self) -> f32 {
+        self.re.to_subset_unchecked()
+    }
+
+    #[inline(always)]
+    fn from_subset(element: &f32) -> Self {
+        // Interpret as a purely real number
+        let re = TSuper::from_subset(element);
+        let eps = SVector::zeros();
+        Self {
+            re,
+            eps,
+            f: PhantomData,
+        }
+    }
+}
+
+impl<TSuper, FSuper, const N: usize> SupersetOf<f64> for DualVec<TSuper, FSuper, N>
+where
+    TSuper: DualNum<FSuper> + SupersetOf<f64>,
+{
+    #[inline(always)]
+    fn is_in_subset(&self) -> bool {
+        self.re.is_in_subset()
+    }
+
+    #[inline(always)]
+    fn to_subset_unchecked(&self) -> f64 {
+        self.re.to_subset_unchecked()
+    }
+
+    #[inline(always)]
+    fn from_subset(element: &f64) -> Self {
+        // Interpret as a purely real number
+        let re = TSuper::from_subset(element);
+        let eps = SVector::zeros();
+        Self {
+            re,
+            eps,
+            f: PhantomData,
+        }
+    }
+}
+
+// We can't do a simd implementation until simba lets us implement SimdPartialOrd
+// using _T_'s SimdBool. The blanket impl gets in the way. So we must constrain
+// T to SimdValue<Element = T, SimdBool = bool>, which is basically the same as
+// saying f32 or f64 only.
+//
+// Limitation of simba. See https://github.com/dimforge/simba/issues/44
+
+use nalgebra::{ComplexField, RealField};
+// This impl is modelled on `impl ComplexField for f32`. The imaginary part is nothing.
+impl<T, const N: usize> ComplexField for DualVec<T, T::Element, N>
+where
+    T: DualNum<T::Element> + SupersetOf<T> + AbsDiffEq<Epsilon = T>,
+    T::Element: DualNum<T::Element> + Scalar + Float,
+    T: SupersetOf<T::Element>,
+    T: SupersetOf<f64>,
+    T: SimdPartialOrd + PartialOrd,
+    T: SimdValue<Element = T, SimdBool = bool>,
+    T: RelativeEq + UlpsEq + AbsDiffEq + From<f64>,
+{
+    type RealField = Self;
+
+    #[inline]
+    fn from_real(re: Self::RealField) -> Self {
+        re
+    }
+
+    #[inline]
+    fn real(self) -> Self::RealField {
+        self
+    }
+
+    #[inline]
+    fn imaginary(self) -> Self::RealField {
+        Self::zero()
+    }
+
+    #[inline]
+    fn modulus(self) -> Self::RealField {
+        self.abs()
+    }
+
+    fn modulus_squared(self) -> Self::RealField {
+        self * self
+    }
+
+    fn argument(self) -> Self::RealField {
+        Self::zero()
+    }
+
+    fn norm1(self) -> Self::RealField {
+        panic!("called norm1() on a dual number")
+    }
+
+    fn scale(self, factor: Self::RealField) -> Self {
+        self * factor
+    }
+
+    fn unscale(self, factor: Self::RealField) -> Self {
+        self / factor
+    }
+
+    fn floor(self) -> Self {
+        panic!("called floor() on a dual number")
+    }
+
+    fn ceil(self) -> Self {
+        panic!("called ceil() on a dual number")
+    }
+
+    fn round(self) -> Self {
+        panic!("called round() on a dual number")
+    }
+
+    fn trunc(self) -> Self {
+        panic!("called trunc() on a dual number")
+    }
+
+    fn fract(self) -> Self {
+        panic!("called fract() on a dual number")
+    }
+
+    fn mul_add(self, a: Self, b: Self) -> Self {
+        DualNum::mul_add(&self, a, b)
+    }
+
+    fn abs(self) -> Self::RealField {
+        Signed::abs(&self)
+    }
+
+    fn hypot(self, other: Self) -> Self::RealField {
+        let sum_sq = self.powi(2) + other.powi(2);
+        DualNum::sqrt(&sum_sq)
+    }
+
+    fn recip(self) -> Self {
+        DualNum::recip(&self)
+    }
+
+    fn conjugate(self) -> Self {
+        self
+    }
+
+    fn sin(self) -> Self {
+        DualNum::sin(&self)
+    }
+
+    fn cos(self) -> Self {
+        DualNum::cos(&self)
+    }
+
+    fn sin_cos(self) -> (Self, Self) {
+        DualNum::sin_cos(&self)
+    }
+
+    fn tan(self) -> Self {
+        DualNum::tan(&self)
+    }
+
+    fn asin(self) -> Self {
+        DualNum::asin(&self)
+    }
+
+    fn acos(self) -> Self {
+        DualNum::acos(&self)
+    }
+
+    fn atan(self) -> Self {
+        DualNum::atan(&self)
+    }
+
+    fn sinh(self) -> Self {
+        DualNum::sinh(&self)
+    }
+
+    fn cosh(self) -> Self {
+        DualNum::cosh(&self)
+    }
+
+    fn tanh(self) -> Self {
+        DualNum::tanh(&self)
+    }
+
+    fn asinh(self) -> Self {
+        DualNum::asinh(&self)
+    }
+
+    fn acosh(self) -> Self {
+        DualNum::acosh(&self)
+    }
+
+    fn atanh(self) -> Self {
+        DualNum::atanh(&self)
+    }
+
+    fn log(self, base: Self::RealField) -> Self {
+        DualNum::ln(&self) / DualNum::ln(&base)
+    }
+
+    fn log2(self) -> Self {
+        DualNum::log2(&self)
+    }
+
+    fn log10(self) -> Self {
+        DualNum::log10(&self)
+    }
+
+    fn ln(self) -> Self {
+        DualNum::ln(&self)
+    }
+
+    fn ln_1p(self) -> Self {
+        DualNum::ln_1p(&self)
+    }
+
+    fn sqrt(self) -> Self {
+        DualNum::sqrt(&self)
+    }
+
+    fn exp(self) -> Self {
+        DualNum::exp(&self)
+    }
+
+    fn exp2(self) -> Self {
+        DualNum::exp2(&self)
+    }
+
+    fn exp_m1(self) -> Self {
+        DualNum::exp_m1(&self)
+    }
+
+    fn powi(self, n: i32) -> Self {
+        DualNum::powi(&self, n)
+    }
+
+    fn powf(self, n: Self::RealField) -> Self {
+        // n could be a dual.
+        DualNum::powd(&self, n)
+    }
+
+    fn powc(self, n: Self) -> Self {
+        // same as powf, Self isn't complex
+        self.powf(n)
+    }
+
+    fn cbrt(self) -> Self {
+        DualNum::cbrt(&self)
+    }
+
+    #[inline]
+    fn is_finite(&self) -> bool {
+        self.re.is_finite()
+    }
+
+    #[inline]
+    fn try_sqrt(self) -> Option<Self> {
+        if self > Self::zero() {
+            Some(DualNum::sqrt(&self))
+        } else {
+            None
+        }
+    }
+}
+
+impl<T, const N: usize> RealField for DualVec<T, T::Element, N>
+where
+    T: DualNum<T::Element> + SupersetOf<T>,
+    T::Element: DualNum<T::Element> + Scalar + Float + From<f64>,
+    T: SupersetOf<T::Element>,
+    T: SupersetOf<f64>,
+    T: SimdPartialOrd + PartialOrd,
+    T: RelativeEq + AbsDiffEq<Epsilon = T>,
+    T: SimdValue<Element = T, SimdBool = bool>,
+    T: UlpsEq,
+    T: AbsDiffEq,
+{
+    fn copysign(self, _sign: Self) -> Self {
+        todo!()
+    }
+
+    fn atan2(self, _other: Self) -> Self {
+        todo!()
+    }
+
+    fn pi() -> Self {
+        Self::from_re(std::f64::consts::PI.into())
+    }
+
+    fn two_pi() -> Self {
+        Self::from_re(std::f64::consts::TAU.into())
+    }
+
+    fn frac_pi_2() -> Self {
+        Self::from_re(std::f64::consts::FRAC_PI_4.into())
+    }
+
+    fn frac_pi_3() -> Self {
+        Self::from_re(std::f64::consts::FRAC_PI_3.into())
+    }
+
+    fn frac_pi_4() -> Self {
+        Self::from_re(std::f64::consts::FRAC_PI_4.into())
+    }
+
+    fn frac_pi_6() -> Self {
+        Self::from_re(std::f64::consts::FRAC_PI_6.into())
+    }
+
+    fn frac_pi_8() -> Self {
+        Self::from_re(std::f64::consts::FRAC_PI_8.into())
+    }
+
+    fn frac_1_pi() -> Self {
+        Self::from_re(std::f64::consts::FRAC_1_PI.into())
+    }
+
+    fn frac_2_pi() -> Self {
+        Self::from_re(std::f64::consts::FRAC_2_PI.into())
+    }
+
+    fn frac_2_sqrt_pi() -> Self {
+        Self::from_re(std::f64::consts::FRAC_2_SQRT_PI.into())
+    }
+
+    fn e() -> Self {
+        Self::from_re(std::f64::consts::E.into())
+    }
+
+    fn log2_e() -> Self {
+        Self::from_re(std::f64::consts::LOG2_E.into())
+    }
+
+    fn log10_e() -> Self {
+        Self::from_re(std::f64::consts::LOG10_E.into())
+    }
+
+    fn ln_2() -> Self {
+        Self::from_re(std::f64::consts::LN_2.into())
+    }
+
+    fn ln_10() -> Self {
+        Self::from_re(std::f64::consts::LN_10.into())
+    }
+
+    fn is_sign_positive(&self) -> bool {
+        self.re.is_sign_positive()
+    }
+
+    fn is_sign_negative(&self) -> bool {
+        self.re.is_sign_negative()
+    }
+
+    fn max(self, other: Self) -> Self {
+        // Got to be careful using this, because you throw away the derivatives of the ones you
+        // don't use.
+        if other > self {
+            other
+        } else {
+            self
+        }
+    }
+
+    fn min(self, other: Self) -> Self {
+        // Got to be careful using this, because you throw away the derivatives of one of them.
+        if other < self {
+            other
+        } else {
+            self
+        }
+    }
+
+    fn clamp(self, min: Self, max: Self) -> Self {
+        // Got to be careful using this, because you throw away the derivatives of one of them.
+        if self < min {
+            min
+        } else if self > max {
+            max
+        } else {
+            self
+        }
+    }
+
+    fn min_value() -> Option<Self> {
+        Some(Self::from_re(T::min_value()))
+    }
+
+    fn max_value() -> Option<Self> {
+        Some(Self::from_re(T::max_value()))
     }
 }
